d27910766e60e0800d3d39d62d8d0f68c3a1b121
ONYEKACHI JOHN <JOHN@onyekachis-mbp.mtn.com.ng>
2017-07-18 14:59:04 -0700

    make it better

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Bottr = require('bottr')
 const BottrApp = require('bottr-app')
 const bot = new Bottr.Bot()
 
-bot.use(new BottrApp())
+bot.use(new Bottr.FacebookMessengerClient())
 bot.on('message_received', function(message, session) {
   session.send('Hello World')
 })
